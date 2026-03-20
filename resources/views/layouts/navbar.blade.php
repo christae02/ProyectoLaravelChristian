d029825ab8f71be7f86c60902151449c65a2930d
christian jesus escobar gomez <chris_tae02@hotmail.com>
2026-03-20 14:17:24 -0600

    Carrito count

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,9 +1,10 @@
 <div class="bg-gray-800">
     <div class="flex">
-        <p class="text-white text-5xl p-[15px] w-[55%]">Antibioticos Farmacias La Providencia</p>
+        <p class="text-white text-5xl p-[15px] w-[55%]">Antibioticos Farmacias La Providencia </p>
         <img class="w-80" src="{{ asset('images/FarmaciaLaProvidencia.png') }}" alt="Logo">
         <div class="w-[20%] flex justify-end">
-            <a class="w-35 h-auto" href="{{ route('carrito.index') }}"><img src="{{ asset('images/carrito.png') }}" alt=""></a>
+            <a class="w-25 h-auto" href="{{ route('carrito.index') }}"><img src="{{ asset('images/carrito.png') }}" alt=""></a>
+            <p class="flex items-center mt-2 p-[2%] bg-white text-lg font-bold rounded-4xl h-[30%]">{{ $countCarrito }}</p>
         </div>
     </div>
 </div>
